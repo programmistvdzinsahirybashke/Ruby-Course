da307a7f438cd565586e5c18a712db15c08e39f6
programmistvdzinsahirybashke <ilnaz09.ys@gmail.com>
2022-07-08 13:54:14 +0300

    1

--- Lesson_11/Dictionary.rb
+++ Lesson_11/Dictionary.rb
@@ -1,14 +1,49 @@
-dict = { "dog" => "собака", "cat" => "кошка", "girl" => "девочка" }
+dict = { "dog" => ["собака", "пес", "песик"], "cat" => ["кошка", "кот"], "girl" => ["девочка", "девушка", "женщина"] }
 
 loop do
-  puts "Введите слово: "
-  eng = gets.strip
+  dict.each do |key, value|
+    puts "Введите слово: "
+    eng = gets.strip
+puts  "Количество переводов слова #{eng}: #{dict[eng].count}"
+    if eng == ""
+      break
+    end
 
-  if eng == ""
-    break
+    "Переводы: #{eng} - #{dict[eng].each_with_index do |i,x|
+        puts "#{x+1}. #{i} "
+        end}"
+
   end
-  puts "Перевод слова #{eng} - #{dict[eng]}"
-
 end
-
 print "Я вас не понял"
+
+# dict = { "dog" => [], "cat" => [], "girl" => [] }
+
+# loop do
+#   puts "Что вы хотите сделать: (1) узнать перевод или (2) добавить слово?"
+#   answer = gets.strip
+
+#   if answer == "1"
+
+#     puts "Введите слово: "
+#     eng = gets.strip
+
+#     if eng == ""
+#       break
+#     end
+
+#     puts "Перевод слова #{eng} - #{dict[eng]}"
+
+#   elsif answer == "2"
+
+#     puts "Введите слово на русском:"
+#     rus = gets.strip
+#     puts "Введите перевод: "
+#     eng = gets.strip
+#     dict[rus] = eng
+
+#   else
+#     print "Я вас не понял"
+#   end
+
+# end
